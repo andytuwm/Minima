e8aa43e7461c3efe11bfb1474e68c6d54565448e
Andy Tu <andytu@alumni.ubc.ca>
2015-08-22 21:15:38 -0700

    adjust minute hand and add circle. decrease letter spacing

--- Wearable/src/main/java/com/example/android/wearable/watchface/MinimaWatchFaceService.java
+++ Wearable/src/main/java/com/example/android/wearable/watchface/MinimaWatchFaceService.java
@@ -144,10 +144,11 @@ public class MinimaWatchFaceService extends CanvasWatchFaceService {
 
             mHourPaint = createTextPaint(mHourDigitsColor, NORMAL_TYPEFACE);
             mHourPaint.setTextAlign(Paint.Align.CENTER);
+            mHourPaint.setLetterSpacing(-0.15f);
 
             mMinutePaint = new Paint();
             mMinutePaint.setARGB(255, 200, 200, 200);
-            mMinutePaint.setStrokeWidth(3.f);
+            mMinutePaint.setStrokeWidth(6f);
             mMinutePaint.setAntiAlias(true);
             mMinutePaint.setStrokeCap(Paint.Cap.ROUND);
 
@@ -287,40 +288,39 @@ public class MinimaWatchFaceService extends CanvasWatchFaceService {
             float centerX = width / 2f;
             float centerY = height / 2f;
 
-            float seconds =
-                    mCalendar.get(Calendar.SECOND) + mCalendar.get(Calendar.MILLISECOND) / 1000f;
-            float secRot = seconds / 60f * TWO_PI;
-            float minutes = mCalendar.get(Calendar.MINUTE);
-            float minRot = minutes / 60f * TWO_PI;
             int hour = mCalendar.get(Calendar.HOUR);
             if (hour == 0) {
                 hour = 12;
             }
             String hourString = String.valueOf(hour);
 
-            float secLength = centerX - 20;
-            float minLength = centerX; // full length
-
-            if (!isInAmbientMode()) {
-                float secX = (float) Math.sin(secRot) * secLength;
-                float secY = (float) -Math.cos(secRot) * secLength;
-                canvas.drawLine(centerX, centerY, centerX + secX, centerY + secY, mSecondPaint);
-            }
-
             // Draw hour text
             canvas.drawText(hourString, centerX, centerY - (mHourPaint.descent() + mHourPaint.ascent()) / 2, mHourPaint);
-
-            // Draw minute hand
-            if (mCalendar.get(Calendar.MINUTE) != 0) {
-                float minX = (float) Math.sin(minRot) * minLength;
-                float minY = (float) -Math.cos(minRot) * minLength;
-                canvas.drawLine(centerX, centerY, centerX + minX, centerY + minY, mMinutePaint);
-            }
 
             // Draw overlay
             canvas.drawArc(0, 0, width, height, startAngle,
                     (mCalendar.get(Calendar.MINUTE) * 6), true, mFadePaint);
             // angles are in degrees: 6 comes from MIN * (2PI / 60) * (180 / PI)
+
+            if (!isInAmbientMode()) {
+                float seconds = 4;
+                float secRot = seconds / 60f * TWO_PI;
+                float secLength = centerX - 20;
+                float secX = (float) Math.sin(secRot) * secLength;
+                float secY = (float) -Math.cos(secRot) * secLength;
+                canvas.drawLine(centerX, centerY, centerX + secX, centerY + secY, mSecondPaint);
+            }
+
+            // Draw minute hand
+            if (mCalendar.get(Calendar.MINUTE) != 0) {
+                float minutes = mCalendar.get(Calendar.MINUTE);
+                float minRot = minutes / 60f * TWO_PI;
+                float minLength = centerX - 5; // full length
+                float minX = (float) Math.sin(minRot) * minLength;
+                float minY = (float) -Math.cos(minRot) * minLength;
+                canvas.drawLine(centerX, centerY, centerX + minX, centerY + minY, mMinutePaint);
+                canvas.drawCircle(centerX, centerY, 7, mMinutePaint);
+            }
 
             // Draw the ticks.
             float innerTickRadius = centerX - 15;
@@ -331,6 +331,7 @@ public class MinimaWatchFaceService extends CanvasWatchFaceService {
                 float tickRadius = innerTickRadius;
                 Paint tickPaint = mTickPaint;
                 if (tickIndex % 5 == 0) {
+                    // Hour ticks
                     tickRadius = innerLargeTickRadius;
                     tickPaint = mLargeTickPaint;
                 }
