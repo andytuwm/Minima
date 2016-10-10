5edb04ccf02921d8c0655f65ef4bb65caba20f4d
Andy Tu <andytu@alumni.ubc.ca>
2016-10-09 23:13:07 -0700

    make util class access local

--- Wearable/src/main/java/com/example/android/wearable/minima/MinimaWatchFaceUtil.java
+++ Wearable/src/main/java/com/example/android/wearable/minima/MinimaWatchFaceUtil.java
@@ -8,18 +8,18 @@ import android.graphics.Typeface;
  * Created by Andy on 2015-08-22.
  */
 
-public final class MinimaWatchFaceUtil {
+final class MinimaWatchFaceUtil {
     private static final String TAG = "MinimaWatchFaceUtil";
 
-    public static final String COLOR_NAME_DEFAULT_AND_AMBIENT_HOUR_DIGITS = "White";
-    public static final int COLOR_VALUE_DEFAULT_AND_AMBIENT_HOUR_DIGITS =
+    private static final String COLOR_NAME_DEFAULT_AND_AMBIENT_HOUR_DIGITS = "White";
+    static final int COLOR_VALUE_DEFAULT_AND_AMBIENT_HOUR_DIGITS =
             parseColor(COLOR_NAME_DEFAULT_AND_AMBIENT_HOUR_DIGITS);
 
     private static int parseColor(String colorName) {
         return Color.parseColor(colorName.toLowerCase());
     }
 
-    public static Paint createTextPaint(int color, Typeface typeface) {
+    static Paint createTextPaint(int color, Typeface typeface) {
         Paint paint = new Paint();
         paint.setColor(color);
         paint.setTypeface(typeface);
